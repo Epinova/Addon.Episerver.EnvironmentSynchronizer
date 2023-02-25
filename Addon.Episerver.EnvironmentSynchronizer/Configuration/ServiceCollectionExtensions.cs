b0432aaa478a4be083678451a1a4542f34e6b2b7
Ove Lartelius <ove_lartelius@hotmail.com>
2023-02-25 21:00:56 +0100

    Added tryCatch to inform better was is wrong.

--- Addon.Episerver.EnvironmentSynchronizer/Configuration/ServiceCollectionExtensions.cs
+++ Addon.Episerver.EnvironmentSynchronizer/Configuration/ServiceCollectionExtensions.cs
@@ -1,17 +1,33 @@
 ﻿using Addon.Episerver.EnvironmentSynchronizer.DynamicData;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System;
+using EPiServer.Logging;
 
 namespace Addon.Episerver.EnvironmentSynchronizer.Configuration
 {
     public static class ServiceCollectionExtensions
     {
+        private static readonly ILogger Logger = LogManager.GetLogger();
+
         public static IServiceCollection AddEnvironmentSynchronization(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddSingleton<IConfigurationReader, ConfigurationReader>();
             services.AddSingleton<EnvironmentSynchronizationManager>();
             services.AddSingleton<EnvironmentSynchronizationStore>();
-            services.AddSingleton(configuration.GetSection(EnvironmentSynchronizerOptions.EnvironmentSynchronizer).Get<EnvironmentSynchronizerOptions>());
+
+            try
+            {
+                services.AddSingleton(configuration.GetSection(EnvironmentSynchronizerOptions.EnvironmentSynchronizer).Get<EnvironmentSynchronizerOptions>());
+            }
+            catch (ArgumentNullException argNullEx)
+            {
+                if (argNullEx.Message.Contains("Value cannot be null. (Parameter 'implementationInstance')"))
+				{
+                    Logger.Error("Addon.EpiServer.EnvironmentSynchronizer tried to load configuration from section EnvironmentSynchronization from appsettings.json looks like it is missing.", argNullEx);
+                }
+                throw;
+            }
 
             return services;
         }
