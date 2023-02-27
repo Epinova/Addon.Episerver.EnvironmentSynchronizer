221bac28dafd0ccf0597415a1dcf467b5fecbc6a
Ove Lartelius <ove_lartelius@hotmail.com>
2023-02-27 16:47:30 +0100

    Test IEnvironmentSynchronizationManager

--- Addon.Episerver.EnvironmentSynchronizer/Configuration/ServiceCollectionExtensions.cs
+++ Addon.Episerver.EnvironmentSynchronizer/Configuration/ServiceCollectionExtensions.cs
@@ -9,8 +9,8 @@ namespace Addon.Episerver.EnvironmentSynchronizer.Configuration
         public static IServiceCollection AddEnvironmentSynchronization(this IServiceCollection services)
         {
             services.AddSingleton<IConfigurationReader, ConfigurationReader>();
-			services.AddSingleton<EnvironmentSynchronizationManager>();
-			services.AddSingleton<EnvironmentSynchronizationStore>();
+			services.AddSingleton<IEnvironmentSynchronizationManager, EnvironmentSynchronizationManager>();
+			services.AddSingleton<IEnvironmentSynchronizationStore, EnvironmentSynchronizationStore>();
 			services.AddSingleton<IInitializationExecuter, InitializationExecuter>();
 			services.AddOptions<EnvironmentSynchronizerOptions>().BindConfiguration(EnvironmentSynchronizerOptions.EnvironmentSynchronizer);
 
