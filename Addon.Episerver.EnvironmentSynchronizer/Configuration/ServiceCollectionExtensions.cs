b72cc2c3e71756ebe5fc20eb6a6a006a11ea65fd
Ove Lartelius <ove_lartelius@hotmail.com>
2023-02-28 23:05:01 +0100

    Fix tread problem

--- Addon.Episerver.EnvironmentSynchronizer/Configuration/ServiceCollectionExtensions.cs
+++ Addon.Episerver.EnvironmentSynchronizer/Configuration/ServiceCollectionExtensions.cs
@@ -9,7 +9,7 @@ namespace Addon.Episerver.EnvironmentSynchronizer.Configuration
         public static IServiceCollection AddEnvironmentSynchronization(this IServiceCollection services)
         {
             services.AddSingleton<IConfigurationReader, ConfigurationReader>();
-			services.AddSingleton<IEnvironmentSynchronizationManager, EnvironmentSynchronizationManager>();
+			services.AddTransient<IEnvironmentSynchronizationManager, EnvironmentSynchronizationManager>(); // Need to be Transient. Otherwise it will pile up result when run schedule jobs.
 			services.AddTransient<IEnvironmentSynchronizationStore, EnvironmentSynchronizationStore>(); // Need to be Transient to be able to store information to database without thread problem.
 			services.AddSingleton<IInitializationExecuter, InitializationExecuter>();
 			services.AddOptions<EnvironmentSynchronizerOptions>().BindConfiguration(EnvironmentSynchronizerOptions.EnvironmentSynchronizer);
