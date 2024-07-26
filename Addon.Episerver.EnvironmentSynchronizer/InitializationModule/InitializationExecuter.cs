2db784d61adffd03e3fe39251d494ceb0500c708
Olivia Axellie <olivia.axellie@epinova.se>
2024-07-26 08:55:18 +0200

    added nullcheck for job before starting it.

--- Addon.Episerver.EnvironmentSynchronizer/InitializationModule/InitializationExecuter.cs
+++ Addon.Episerver.EnvironmentSynchronizer/InitializationModule/InitializationExecuter.cs
@@ -10,80 +10,90 @@ using EPiServer.ServiceLocation;
 
 namespace Addon.Episerver.EnvironmentSynchronizer.InitializationModule
 {
-	public interface IInitializationExecuter
-	{
-		void Initialize();
-	}
+    public interface IInitializationExecuter
+    {
+        void Initialize();
+    }
 
-	[ServiceConfiguration(typeof(IInitializationExecuter))]
-	public class InitializationExecuter : IInitializationExecuter
-	{
-		private static readonly ILogger Logger = LogManager.GetLogger();
+    [ServiceConfiguration(typeof(IInitializationExecuter))]
+    public class InitializationExecuter : IInitializationExecuter
+    {
+        private static readonly ILogger Logger = LogManager.GetLogger();
 
-		private readonly IScheduledJobRepository _scheduledJobRepository;
-		private readonly IConfigurationReader _configurationReader;
-		private readonly IScheduledJobExecutor _scheduledJobExecutor;
-		private readonly IEnvironmentSynchronizationManager _environmentSynchronizationManager;
-		private readonly IEnvironmentSynchronizationStore _environmentSynchronizationStore;
+        private readonly IScheduledJobRepository _scheduledJobRepository;
+        private readonly IConfigurationReader _configurationReader;
+        private readonly IScheduledJobExecutor _scheduledJobExecutor;
+        private readonly IEnvironmentSynchronizationManager _environmentSynchronizationManager;
+        private readonly IEnvironmentSynchronizationStore _environmentSynchronizationStore;
 
-		public InitializationExecuter(
-			IScheduledJobRepository scheduledJobRepository, 
-			IConfigurationReader configurationReader, 
-			IScheduledJobExecutor scheduledJobExecutor, 
-			IEnvironmentSynchronizationManager environmentSynchronizationManager, 
-			IEnvironmentSynchronizationStore environmentSynchronizationStore)
-		{
-			_scheduledJobRepository = scheduledJobRepository;
-			_configurationReader = configurationReader;
-			_scheduledJobExecutor = scheduledJobExecutor;
-			_environmentSynchronizationManager = environmentSynchronizationManager;	
-			_environmentSynchronizationStore = environmentSynchronizationStore;
-		}
-
-		public void Initialize()
+        public InitializationExecuter(
+            IScheduledJobRepository scheduledJobRepository,
+            IConfigurationReader configurationReader,
+            IScheduledJobExecutor scheduledJobExecutor,
+            IEnvironmentSynchronizationManager environmentSynchronizationManager,
+            IEnvironmentSynchronizationStore environmentSynchronizationStore)
         {
-			var syncData = _configurationReader.ReadConfiguration();
+            _scheduledJobRepository = scheduledJobRepository;
+            _configurationReader = configurationReader;
+            _scheduledJobExecutor = scheduledJobExecutor;
+            _environmentSynchronizationManager = environmentSynchronizationManager;
+            _environmentSynchronizationStore = environmentSynchronizationStore;
+        }
 
-			if (syncData.RunAsInitializationModule)
-			{
-				Logger.Information($"Environment Synchronizer found RunAsInitializationModule=true");
-				var runInitialization = true;
+        public void Initialize()
+        {
+            var syncData = _configurationReader.ReadConfiguration();
 
-				if (!syncData.RunInitializationModuleEveryStartup)
-				{
-					Logger.Information($"Environment Synchronizer found RunInitializationModuleEveryStartup=false");
-					var stamp = _environmentSynchronizationStore.GetStamp();
-					if (stamp != null && stamp.Environment == _environmentSynchronizationManager.GetEnvironmentName())
-					{
-						runInitialization = false;
-						Logger.Information($"Environment Synchronizer will not run. Stamp match the current environment {stamp.Environment}");
-					}
-				}
+            if (syncData.RunAsInitializationModule)
+            {
+                Logger.Information($"Environment Synchronizer found RunAsInitializationModule=true");
+                var runInitialization = true;
 
-				if (!runInitialization) { return; }
+                if (!syncData.RunInitializationModuleEveryStartup)
+                {
+                    Logger.Information($"Environment Synchronizer found RunInitializationModuleEveryStartup=false");
+                    var stamp = _environmentSynchronizationStore.GetStamp();
+                    if (stamp != null && stamp.Environment == _environmentSynchronizationManager.GetEnvironmentName())
+                    {
+                        runInitialization = false;
+                        Logger.Information($"Environment Synchronizer will not run. Stamp match the current environment {stamp.Environment}");
+                    }
+                }
 
-				try
-				{
-					var jobId =
-						((ScheduledPlugInAttribute) typeof(EnvironmentSynchronizationJob).GetCustomAttributes(
-							typeof(ScheduledPlugInAttribute), true)[0]).GUID;
-					var job = _scheduledJobRepository.Get(Guid.Parse(jobId));
-					_scheduledJobExecutor.StartAsync(job,
-                        new JobExecutionOptions
-                        {
-                            RunSynchronously = true,
-                            Trigger = ScheduledJobTrigger.User
-                        });
-				}
-				catch (Exception ex)
-				{
-					Logger.Error("Could not get find or load EnvironmentSynchronizationJob. SynchronizationInitializationModule will not run.", ex);
-				}
-			} else
-			{
-				Logger.Information($"Environment Synchronizer found RunAsInitializationModule=false");
-			}
-		}
+                if (!runInitialization) { return; }
+
+                try
+                {
+                    var jobId =
+                        ((ScheduledPlugInAttribute)typeof(EnvironmentSynchronizationJob).GetCustomAttributes(
+                            typeof(ScheduledPlugInAttribute), true)[0]).GUID;
+                    var job = _scheduledJobRepository.Get(Guid.Parse(jobId));
+
+                    if (job is not null)
+                    {
+                        _scheduledJobExecutor.StartAsync(job,
+                            new JobExecutionOptions
+                            {
+                                RunSynchronously = true,
+                                Trigger = ScheduledJobTrigger.User
+                            });
+                    }
+                    else
+                    {
+                        Logger.Error(
+                            "Could not get find or load EnvironmentSynchronizationJob. SynchronizationInitializationModule will not run.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Logger.Error("Could not get find or load EnvironmentSynchronizationJob. SynchronizationInitializationModule will not run.", ex);
+                }
+            }
+            else
+            {
+                Logger.Information($"Environment Synchronizer found RunAsInitializationModule=false");
+            }
+        }
 
 
     }
